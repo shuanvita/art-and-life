b01e573020a77a206f1872ed2e3ead7999cfc568
Andrey Shurygin <shuanvita@gmail.com>
2026-05-07 19:57:09 +0300

    Настройка проекта

---
+++ app/pages/magazine.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div>Magazine page</div>
+</template>
+
+<style scoped>
+
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,26 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+
+  css: ['~/assets/styles/main.css'],
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/image',
     '@nuxt/scripts'
-  ]
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      include: [
+        '@vue/devtools-core',
+        '@vue/devtools-kit'
+      ]
+    }
+  }
 })
